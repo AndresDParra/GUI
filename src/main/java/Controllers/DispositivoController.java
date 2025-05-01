a1cebc2b31e642c8f00669d1b7b17001ce3daffc
Esteban06yt <133072260+Esteban06yt@users.noreply.github.com>
2025-05-01 01:52:08 -0500

    interfaces casi terminadas

--- src/main/java/Controllers/DispositivoController.java
+++ src/main/java/Controllers/DispositivoController.java
@@ -1,4 +1,20 @@
 package Controllers;
 
+import javafx.event.ActionEvent;
+
 public class DispositivoController {
+    public void AgregarDispositivo(ActionEvent actionEvent) {
+    }
+
+    public void AgregarModulo(ActionEvent actionEvent) {
+    }
+
+    public void FiltrarConModulos(ActionEvent actionEvent) {
+    }
+
+    public void OrdenarPorPrioridad(ActionEvent actionEvent) {
+    }
+
+    public void ContarPorTipo(ActionEvent actionEvent) {
+    }
 }
